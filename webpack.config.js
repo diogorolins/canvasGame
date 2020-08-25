165fa8680a7003cf414837dc00bceeaf2cb5c3d0
= <=>
2020-08-25 17:33:09 -0300

    adding static.json

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./canvas.js",
   output: {
     path: path.resolve(__dirname, "dist"),
